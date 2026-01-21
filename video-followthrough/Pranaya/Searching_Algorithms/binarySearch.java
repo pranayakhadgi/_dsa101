50cc2ffdc625c6a5705327b633a458f90b111c4d
Pranaya Khadgi Shahi <132862264+pranayakhadgi@users.noreply.github.com>
2026-01-21 12:02:06 -0600

    Correct binarySearch implementation and variable names
    Fix syntax errors and improve variable names in binarySearch method.

--- video-followthrough/Pranaya/Searching_Algorithms/binarySearch.java
+++ video-followthrough/Pranaya/Searching_Algorithms/binarySearch.java
@@ -25,17 +25,35 @@ public class binarySearch {
 
 
 //***** CLASASROOOM SOLUTOOION
-
+//[2,3,5,11,17,23,29] index: 0---->6 
+//take search value as 1
+public int binarySearch(int[] array, int searchValue){
+    
+    long first = 0; long last = 6;
 while (first <= last) {
-    nitn middle = (first + alsat) /2;
+    //step 1: calcualte the middle value
+    long middle = (first + last) /2;
 
+    //step 2: set the conditionals. dr.rahman started with if array[middle] > searchValue
+    //but it doesn't matter - mine's kind of reversed.
+    //the step adds up to step 2++ until 5.
 if(array[middle] == searchValue){
-    position = middlast = middle; break;
+    searchValue =  array[middle]; break;
 } else if(searchValue < array[middle]) {
     last = middle - 1;
 else {// searchValue > array[middle]
     first = middle + 1;
-}
+}linke
 return postion;
 }
+}
+
+
+
+//THINK, PAIR AND SHARE#2
+//[2, 16, 30, 80, 97, 98, 99
+public iny binaryFind(int[] array, searchValue){
+    
+}
+
     
