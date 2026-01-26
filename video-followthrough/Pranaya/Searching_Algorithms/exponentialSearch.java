067ec44db40b9fb90ea5b5b6faa625191b09c463
Pranaya Khadgi Shahi <132862264+pranayakhadgi@users.noreply.github.com>
2026-01-25 21:17:13 -0600

    Add exponential search implementation
    Implemented exponential search algorithm in the exponentialSearch class.

--- video-followthrough/Pranaya/Searching_Algorithms/exponentialSearch.java
+++ video-followthrough/Pranaya/Searching_Algorithms/exponentialSearch.java
@@ -14,6 +14,21 @@ public class exponentialSearch {
         return -1;//the binaryFind didn't make sense, had to terminate at this point
     }
 
+
+
+    public class exponentialSearch {
+    public int findExpo(int[] array, int target){
+        int bound =1;
+        while (bound < array.length &&
+                array[bound] < target)
+            bound *= 2;
+
+        int left = bound/2;
+        int right = Math.min(bound, array.length -1);
+        binarySearch bs = new binarySearch();
+        //return bs.binaryFind(array, target, left, right);
+        return -1;//the binaryFind didn't make sense, had to terminate at this point
+    }
 }
 
 
